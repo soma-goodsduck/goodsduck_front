a5c4dda618b97281482118f72cf10982eb1e4485
2dowon <dwon424@gmail.com>
2021-07-03 01:02:26 +0900

    💄 [style] elements(button, grid, image, input, text)

---
+++ src/elements/button.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = (props) => {
+  const {
+    text,
+    width,
+    height,
+    borderRadius,
+    padding,
+    margin,
+    bg,
+    color,
+    src,
+    _onClick,
+  } = props;
+
+  const styles = {
+    text: text,
+    width: width,
+    height: height,
+    borderRadius: borderRadius,
+    padding: padding,
+    margin: margin,
+    bg: bg,
+    color: color,
+    src: src,
+  };
+
+  return (
+    <>
+      <ElButton {...styles} onClick={_onClick}>
+        {text}
+      </ElButton>
+    </>
+  );
+};
+
+Button.defaultProps = {
+  text: "",
+  width: "100%",
+  height: "100%",
+  borderRadius: "",
+  padding: "",
+  margin: "",
+  bg: "black",
+  color: "white",
+  src: "",
+  _onClick: () => {},
+};
+
+const ElButton = styled.button`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  border-radius: ${(props) => props.borderRadius};
+  padding: ${(props) => props.padding};
+  margin: ${(props) => props.margin};
+  background-color: ${(props) => props.bg};
+  color: ${(props) => props.color};
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+  cursor: pointer;
+  border: none;
+`;
+
+export default Button;
---
+++ src/elements/index.js
@@ -0,0 +1,7 @@
+import Grid from "./grid";
+import Image from "./image";
+import Text from "./text";
+import Input from "./input";
+import Button from "./button";
+
+export { Grid, Image, Text, Input, Button };
