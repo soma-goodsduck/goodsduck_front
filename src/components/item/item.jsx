94a888ca4265872b69bd41326319dd91fb610e5a
2dowon <dwon424@gmail.com>
2021-07-10 00:48:37 +0900

    ✨ [feat] show good items

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,66 +1,93 @@
 import React from "react";
+import styles from "./item.module.css";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
+
 import { Image, Grid, Flex, Text } from "../../elements";
 
-const Item = (props) => {
+const Item = ({ item, id, onHeartClick }) => {
+  const isLike = useSelector((state) => state.item.is_like);
+
+  const clickHeart = () => {
+    onHeartClick(id);
+  };
+
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
   return (
-    <Grid is_flex_col>
-      <Flex>
+    <Grid is_flex_col margin="10px 0 0 0">
+      <Flex className={styles.imgBox}>
         <Image
           shape="rectangle"
-          src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg"
+          src={item.image_url}
           size={window.screen.width > 400 ? "185px" : "43vw"}
           borderRadius="10px"
+          className={styles.itemImg}
+        />
+        <button
+          className={isLike ? styles.clickLikeBtn : styles.likeBtn}
+          onClick={() => clickHeart()}
         />
       </Flex>
-      <Flex justify="flex-start" padding="10px 7px">
-        <Text
-          size={window.screen.width > 400 ? "16px" : "4vw"}
-          color="#299bff"
-          bold
-          margin="0 5px 0 0"
-        >
-          구매
-        </Text>
-        <Title>
-          <Text size={window.screen.width > 400 ? "16px" : "4vw"} is_long>
-            GD 포토카드 팔아요
+      <InfoBox>
+        <Flex justify="flex-start" padding="5px">
+          <Text
+            size={window.screen.width > 400 ? "16px" : "4vw"}
+            bold
+            margin="0 5px 0 0"
+            color={item.trade_type === "구매" ? "#299bff" : "#e15b5b"}
+          >
+            {item.trade_type}
           </Text>
-        </Title>
-      </Flex>
-      <Flex justify="flex-start" padding=" 0 7px">
-        <Text size={window.screen.width > 400 ? "18px" : "5vw"} bold>
-          20,000원
-        </Text>
-      </Flex>
-      <Flex justify="space-between" padding="5px 10px 20px 5px">
-        <Flex>
-          <Image
-            shape="circle"
-            src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg"
-            size={window.screen.width > 400 ? "24px" : "7vw"}
-            margin="0 7px 0 0"
-          />
-          <UserName>
-            <Text is_long size={window.screen.width > 400 ? "18px" : "4.5vw"}>
-              GDDDD
+          <Title>
+            <Text size={window.screen.width > 400 ? "16px" : "4vw"} is_long>
+              {item.name}
             </Text>
-          </UserName>
+          </Title>
         </Flex>
-        <Text color="#bbbbbb" size={window.screen.width > 400 ? "16px" : "4vw"}>
-          47분전
-        </Text>
-      </Flex>
+        <Flex justify="flex-start" padding=" 0 7px">
+          <Text size={window.screen.width > 400 ? "18px" : "5vw"} bold>
+            {numberWithCommas(item.price)}원
+          </Text>
+        </Flex>
+        <Flex justify="space-between" padding="5px 10px 20px 5px">
+          <Flex>
+            <Image
+              shape="circle"
+              src={item.user_image}
+              size={window.screen.width > 400 ? "24px" : "6.5vw"}
+              margin="0 7px 0 0"
+            />
+            <UserName>
+              <Text is_long size={window.screen.width > 400 ? "18px" : "4.5vw"}>
+                {item.user_name}
+              </Text>
+            </UserName>
+          </Flex>
+          <Text
+            color="#bbbbbb"
+            size={window.screen.width > 400 ? "16px" : "4vw"}
+          >
+            {item.item_created_at}분전
+          </Text>
+        </Flex>
+      </InfoBox>
     </Grid>
   );
 };
 
 const Title = styled.div`
-  width: 80%;
+  width: 75%;
 `;
 
 const UserName = styled.div`
   width: 65px;
+`;
+
+const InfoBox = styled.div`
+  padding: 5px;
 `;
 
 export default Item;
