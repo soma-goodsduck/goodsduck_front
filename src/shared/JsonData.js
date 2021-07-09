71349f19a025a2c06509a5d83a2e2d503c32ca46
2dowon <dwon424@gmail.com>
2021-07-10 00:49:54 +0900

    📝 [chore] mockup data

--- src/shared/JsonData.js
+++ src/shared/JsonData.js
@@ -14,14 +14,24 @@ export const idols = [
     engName: "SHINee",
     img: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile30.uf.tistory.com%2Fimage%2F24395443582687C3196284",
   },
-  //   {
-  //     korName: "트와이스",
-  //     engName: "Twice",
-  //     img: "https://image.newsis.com/2020/10/26/NISI20201026_0000623685_web.jpg?rnd=20201026080838",
-  //   },
-  //   {
-  //     korName: "세븐틴",
-  //     engName: "Seventeen",
-  //     img: "http://www.beffreport.com/news/photo/202012/88911_43486_4938.jpg",
-  //   },
+  {
+    korName: "트와이스",
+    engName: "Twice",
+    img: "https://image.newsis.com/2020/10/26/NISI20201026_0000623685_web.jpg?rnd=20201026080838",
+  },
+  {
+    korName: "세븐틴",
+    engName: "Seventeen",
+    img: "http://www.beffreport.com/news/photo/202012/88911_43486_4938.jpg",
+  },
+  {
+    korName: "오마이걸",
+    engName: "Oh My Girl",
+    img: "https://newsimg.sedaily.com/2021/04/21/22L657OAYV_1.jpg",
+  },
+  {
+    korName: "NCT DREAM",
+    engName: "NCT DREAM",
+    img: "http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/080/542/006/80542006_1624517573514_8_600x600.JPG",
+  },
 ];
---
+++ src/shared/JsonDataItem.js
@@ -0,0 +1,109 @@
+export const items = [
+  {
+    id: 1, // 굿즈 아이디
+    idol_member_id: null, // 아이돌 멤버
+    category_item_id: null, // 카테고리
+    trade_status: "구매중", // 거래 상태
+    trade_type: "구매", // 거래 타입
+    status: "", // 상품 상태
+    name: "GD 포토카드 사요", // 제목
+    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg", // 굿즈 이미지
+    price: 20000, // 굿즈 가격
+    user_name: "GDDDD", // 글쓴이 아이디
+    user_image:
+      "https://pbs.twimg.com/profile_images/1236934708405006337/DnWgP_0M_400x400.jpg", // 글쓴이 프로필 이미지
+    item_created_at: 47, // 글 생성 시각
+    is_like: false, // 유저가 해당 아이템을 찜했는지 여부
+  },
+  {
+    id: 2,
+    idol_member_id: null,
+    category_item_id: null,
+    trade_status: "판매중",
+    trade_type: "판매",
+    status: "",
+    name: "GD 포토카드 팔아요",
+    image_url:
+      "https://pbs.twimg.com/profile_images/1236934708405006337/DnWgP_0M_400x400.jpg",
+    price: 10000,
+    user_name: "GDADFASDFASF",
+    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    item_created_at: 147,
+    is_like: false,
+  },
+  {
+    id: 3,
+    idol_member_id: null,
+    category_item_id: null,
+    trade_status: "판매중",
+    trade_type: "판매",
+    status: "",
+    name: "GD 포스터",
+    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    price: 20000,
+    user_name: "GDDDD",
+    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    item_created_at: 47,
+    is_like: false,
+  },
+  {
+    id: 4,
+    idol_member_id: null,
+    category_item_id: null,
+    trade_status: "구매중",
+    trade_type: "구매",
+    status: "",
+    name: "GD 포토카드 사요",
+    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    price: 20000,
+    user_name: "GDDDD",
+    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    item_created_at: 47,
+    is_like: false,
+  },
+  {
+    id: 5,
+    idol_member_id: null,
+    category_item_id: null,
+    trade_status: "구매중",
+    trade_type: "구매",
+    status: "",
+    name: "GD 포토카드 사요",
+    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    price: 20000,
+    user_name: "GDDDD",
+    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    item_created_at: 47,
+    is_like: false,
+  },
+  {
+    id: 6,
+    idol_member_id: null,
+    category_item_id: null,
+    trade_status: "구매중",
+    trade_type: "구매",
+    status: "",
+    name: "GD 포토카드 사요",
+    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    price: 20000,
+    user_name: "GDDDD",
+    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    item_created_at: 47,
+    is_like: false,
+  },
+  {
+    id: 7,
+    idol_member_id: null,
+    category_item_id: null,
+    trade_status: "구매중",
+    trade_type: "구매",
+    status: "",
+    name: "GD 포토카드 사요",
+    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    price: 20000,
+    user_name: "GDDDD",
+    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    item_created_at: 47,
+    is_like: false,
+  },
+];
