d8626b5ccb28c8a96bbfb3aca4dc359ae43e48a2
2dowon <dwon424@gmail.com>
2021-07-12 01:40:51 +0900

    [chore] update mockup data

--- src/shared/JsonDataItem.js
+++ src/shared/JsonDataItem.js
@@ -12,7 +12,7 @@ export const items = [
     user_name: "GDDDD", // 글쓴이 아이디
     user_image:
       "https://pbs.twimg.com/profile_images/1236934708405006337/DnWgP_0M_400x400.jpg", // 글쓴이 프로필 이미지
-    item_created_at: 47, // 글 생성 시각
+    item_created_at: 2, // 글 생성 시각
     is_like: false, // 유저가 해당 아이템을 찜했는지 여부
   },
   {
@@ -22,13 +22,13 @@ export const items = [
     trade_status: "판매중",
     trade_type: "판매",
     status: "",
-    name: "GD 포토카드 팔아요",
-    image_url:
-      "https://pbs.twimg.com/profile_images/1236934708405006337/DnWgP_0M_400x400.jpg",
-    price: 10000,
-    user_name: "GDADFASDFASF",
-    user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
-    item_created_at: 147,
+    name: "NCT 마크 포토카드 팔아요",
+    image_url: "https://pbs.twimg.com/media/D8JNRkpUwAACjz6.jpg",
+    price: 7000,
+    user_name: "마크크크",
+    user_image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv4n-yPPCpcpZgY7wYjb8uYHip7A4N05sx9w&usqp=CAU",
+    item_created_at: 47,
     is_like: false,
   },
   {
@@ -38,12 +38,12 @@ export const items = [
     trade_status: "판매중",
     trade_type: "판매",
     status: "",
-    name: "GD 포스터",
-    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
-    price: 20000,
+    name: "GD 포스터 판매",
+    image_url: "https://purme.org/wp-content/uploads/20170214_085521.jpg",
+    price: 100000,
     user_name: "GDDDD",
     user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
-    item_created_at: 47,
+    item_created_at: 147,
     is_like: false,
   },
   {
@@ -54,11 +54,12 @@ export const items = [
     trade_type: "구매",
     status: "",
     name: "GD 포토카드 사요",
-    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    image_url:
+      "https://nimage.g-enews.com/phpwas/restmb_allidxmake.php?idx=5&simg=201506011138446473545_20150601113900_01.jpg",
     price: 20000,
     user_name: "GDDDD",
     user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
-    item_created_at: 47,
+    item_created_at: 151,
     is_like: false,
   },
   {
@@ -68,8 +69,9 @@ export const items = [
     trade_status: "구매중",
     trade_type: "구매",
     status: "",
-    name: "GD 포토카드 사요",
-    image_url: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
+    name: "포토카드 사요",
+    image_url:
+      "https://ssl.pstatic.net/sstatic/people/portrait/202010/20201015104158808-4251462.jpg",
     price: 20000,
     user_name: "GDDDD",
     user_image: "https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/gd.jpg",
