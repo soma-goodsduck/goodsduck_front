54edbf0bff606641fb963b4654d2a0f0c00f6884
2dowon <dwon424@gmail.com>
2021-07-18 12:12:07 +0900

    🎨 [chore] refactoring idol group filtering

--- src/components/idolFiltering/idolGroupFiltering.jsx
+++ src/components/idolFiltering/idolGroupFiltering.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import styled from "styled-components";
 import styles from "./idolGroupFiltering.module.css";
 import { Flex, Text, Image } from "../../elements";
 
-import Idol from "./idolFiltering";
 import idols from "../../shared/IdolGroupData.json";
 
+import { actionCreators as headerActions } from "../../redux/modules/header";
+
 const IdolGroupFiltering = () => {
+  const dispatch = useDispatch();
+
+  const [groupId, setGroupId] = useState(0);
+  const isFiltering = useSelector((state) => state.header.click_filtering);
+
+  const checkGroupHandler = (id) => {
+    setGroupId(id);
+    dispatch(headerActions.clickfilteringAction(isFiltering));
+  };
+
   const scrollRef = useRef(null);
 
   const [isDrag, setIsDrag] = useState(false);
@@ -41,13 +53,39 @@ const IdolGroupFiltering = () => {
     >
       <Flex justify="flex-start">
         {idols.map((idol) => (
-          <Idol key={idol.engName} idol={idol} className={styles.category} />
+          <IdolBox key={idol.id}>
+            <IdolInput
+              id={idol.id}
+              type="radio"
+              checked={groupId === idol.id}
+              onChange={() => checkGroupHandler(idol.id)}
+            />
+            <label
+              htmlFor={idol.id}
+              className={
+                groupId === idol.id
+                  ? styles.clickIdolGroupBtn
+                  : styles.idolGroupBtn
+              }
+            >
+              <img
+                className={
+                  groupId === idol.id
+                    ? styles.clickIdolGroupImg
+                    : styles.idolGroupImg
+                }
+                src={idol.imageUrl}
+                alt="Idol Group"
+              />
+              {idol.name}
+            </label>
+          </IdolBox>
         ))}
         <BtnBox>
           <AddBtn>
             <Image
               src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_add.svg"
-              size="40px"
+              size="30px"
             />
           </AddBtn>
           <Text size="13px" margin="10px 0 0 0" color="#222222">
@@ -59,21 +97,27 @@ const IdolGroupFiltering = () => {
   );
 };
 
+const IdolBox = styled.div`
+  padding: 0 10px;
+  text-align: center;
+`;
+const IdolInput = styled.input`
+  display: none;
+`;
+
 const BtnBox = styled.button`
   width: 70px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  cursor: pointer;
 `;
 
 const AddBtn = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 50px;
-  height: 50px;
+  width: 60px;
+  height: 60px;
   border-radius: 50%;
   background-color: #f8f8f8;
 `;
