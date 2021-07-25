255e26030b4621bb3ac6287f6163586f23a94cd8
2dowon <dwon424@gmail.com>
2021-07-26 01:34:29 +0900

    ✨ [feat] infinite scroll

--- src/components/itemList/itemList.jsx
+++ src/components/itemList/itemList.jsx
@@ -1,37 +1,84 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable consistent-return */
+import React, { useEffect, useState, useCallback } from "react";
 import { useSelector } from "react-redux";
 
 import styled from "styled-components";
 import styles from "./itemList.module.css";
 import Item from "../item/item";
 
-import { getInfo } from "../../shared/axios";
+import { getList } from "../../shared/axios";
 
 const ItemList = () => {
   const isFiltering = useSelector((state) => state.header.click_filtering);
 
   // 해당 아이템 데이터 받아오기
   const [items, setItems] = useState([]);
+  const [hasNext, setHasNext] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [pageNum, setPageNum] = useState(0);
+
+  const getItems = (num) => {
+    setLoading(true);
+    const getItemList = getList("items", num);
+    getItemList.then((result) => {
+      const newItem = [...items, ...result.response.content];
+      setItems(newItem);
+      setHasNext(result.hasNext);
+      setLoading(false);
+      setPageNum(pageNum + 1);
+    });
+  };
 
   useEffect(() => {
-    const getItemList = getInfo("items");
-    getItemList.then((result) => {
-      setItems(result);
-    });
+    getItems(pageNum);
   }, []);
 
-  if (!items) {
-    return null;
-  }
+  // 무한 스크롤
+  const infiniteScroll = useCallback(() => {
+    const scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight,
+    );
+    const scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop,
+    );
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollHeight === scrollTop + clientHeight) {
+      if (loading) {
+        return;
+      }
+      getItems(pageNum);
+    }
+  }, [pageNum]);
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+
+    if (hasNext) {
+      window.addEventListener("scroll", infiniteScroll, true);
+    } else {
+      window.removeEventListener("scroll", infiniteScroll, true);
+    }
+
+    return () => window.removeEventListener("scroll", infiniteScroll, true);
+  }, [hasNext, pageNum]);
 
   return (
-    <ItemListBox
-      className={isFiltering ? styles.filtering : styles.nonFiltering}
-    >
-      {items.map((item) => (
-        <Item key={item.name} id={item.itemId} item={item} />
-      ))}
-    </ItemListBox>
+    <>
+      {items && (
+        <ItemListBox
+          className={isFiltering ? styles.filtering : styles.nonFiltering}
+        >
+          {items.map((item) => (
+            <Item key={item.itemId} id={item.itemId} item={item} />
+          ))}
+        </ItemListBox>
+      )}
+    </>
   );
 };
 
--- src/shared/axios.js
+++ src/shared/axios.js
@@ -2,6 +2,28 @@
 import axios from "axios";
 import * as Sentry from "@sentry/react";
 
+// 무한 스크롤 (홈 데이터)
+export const getList = async (path, pageNumber) => {
+  const jwt = localStorage.getItem("jwt");
+
+  try {
+    const result = await axios.get(
+      `${process.env.REACT_APP_BACK_URL}/api/v1/${path}?pageNumber=${pageNumber}`,
+      {
+        headers: { jwt: `${jwt}` },
+      },
+    );
+    if (result.headers.jwt) {
+      localStorage.setItem("jwt", result.headers.jwt);
+    }
+
+    return result.data;
+  } catch (error) {
+    console.log("error", error);
+    Sentry.captureException(error);
+  }
+};
+
 // JWT를 리턴하지 않는 데이터 (비회원 가능)
 export const getInfo = async (path) => {
   const jwt = localStorage.getItem("jwt");
@@ -46,6 +68,34 @@ export const getData = async (path) => {
     }
 
     return result.data.response;
+  } catch (error) {
+    console.log("error", error);
+    Sentry.captureException(error);
+  }
+};
+
+// 회원인지, 비회원인지 체크
+export const checkLoginWithData = async (path) => {
+  const jwt = localStorage.getItem("jwt");
+
+  try {
+    const result = await axios.get(
+      `${process.env.REACT_APP_BACK_URL}/api/v1/${path}`,
+      {
+        headers: { jwt: `${jwt}` },
+      },
+    );
+    if (result.data.error !== null) {
+      if (result.data.error.status === 401) {
+        // 로그인 만료
+        return "login";
+      }
+    }
+    if (result.headers.jwt) {
+      localStorage.setItem("jwt", result.headers.jwt);
+    }
+
+    return result.data;
   } catch (error) {
     console.log("error", error);
     Sentry.captureException(error);
@@ -104,6 +154,95 @@ export const deleteAction = async (path) => {
     }
 
     return result.data.response;
+  } catch (error) {
+    console.log("error", error);
+    Sentry.captureException(error);
+  }
+};
+
+// post 요청
+export const postAction = async (path, json) => {
+  const jwt = localStorage.getItem("jwt");
+
+  try {
+    const result = await axios.post(
+      `${process.env.REACT_APP_BACK_URL}/api/v1/${path}`,
+      JSON.stringify(json),
+      {
+        headers: { jwt: `${jwt}`, "Content-Type": "application/json" },
+      },
+    );
+    if (result.data.error !== null) {
+      if (result.data.error.status === 401) {
+        // 로그인 만료
+        return "login";
+      }
+    }
+
+    if (result.headers.jwt) {
+      localStorage.setItem("jwt", result.headers.jwt);
+    }
+
+    return result.data;
+  } catch (error) {
+    console.log("error", error);
+    Sentry.captureException(error);
+  }
+};
+
+// patch 요청
+export const patchAction = async (path, json) => {
+  const jwt = localStorage.getItem("jwt");
+
+  try {
+    const result = await axios.patch(
+      `${process.env.REACT_APP_BACK_URL}/api/v1/${path}`,
+      {
+        headers: { jwt: `${jwt}` },
+      },
+    );
+    if (result.data.error !== null) {
+      if (result.data.error.status === 401) {
+        // 로그인 만료
+        return "login";
+      }
+    }
+
+    if (result.headers.jwt) {
+      localStorage.setItem("jwt", result.headers.jwt);
+    }
+
+    return result.data;
+  } catch (error) {
+    console.log("error", error);
+    Sentry.captureException(error);
+  }
+};
+
+// patch JSON 요청
+export const patchJsonAction = async (path, json) => {
+  const jwt = localStorage.getItem("jwt");
+
+  try {
+    const result = await axios.patch(
+      `${process.env.REACT_APP_BACK_URL}/api/v1/${path}`,
+      JSON.stringify(json),
+      {
+        headers: { jwt: `${jwt}`, "Content-Type": "application/json" },
+      },
+    );
+    if (result.data.error !== null) {
+      if (result.data.error.status === 401) {
+        // 로그인 만료
+        return "login";
+      }
+    }
+
+    if (result.headers.jwt) {
+      localStorage.setItem("jwt", result.headers.jwt);
+    }
+
+    return result.data;
   } catch (error) {
     console.log("error", error);
     Sentry.captureException(error);
