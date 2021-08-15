ea63fc3704bf2e54f909ae80d9206808bc7a2ff1
2dowon <dwon424@gmail.com>
2021-08-15 23:05:23 +0900

    ✨ [feat] show notification list

--- src/pages/notification/notificationPage.jsx
+++ src/pages/notification/notificationPage.jsx
@@ -1,43 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import HeaderInfo from "../../components/haeder/headerInfo";
 import LoginPopUp from "../../elements/loginPopUp";
 import NotificationRow from "./notificationRow";
+import { requestAuthData } from "../../shared/axios";
 
 const NotificationPage = () => {
   const [showPopup, setShowPopup] = useState(false);
+  const [notifications, setNotifications] = useState(null);
 
-  const notifications = [
-    {
-      id: 1,
-      type: "price-propose",
-      user: {
-        nickName: "밍밍",
-        imageUrl:
-          "https://newsimg.hankookilbo.com/cms/articlerelease/2020/11/23/51c796f6-55b3-496d-8572-ce98d97ea038.jpg",
-      },
-      item: {
-        name: "시우민 포스터 팔아여",
-      },
-      proposedPrice: 15000,
-      createdAt: 1627799751243,
-    },
-    {
-      id: 2,
-      type: "review",
-      user: {
-        nickName: "밍밍",
-        imageUrl:
-          "https://newsimg.hankookilbo.com/cms/articlerelease/2020/11/23/51c796f6-55b3-496d-8572-ce98d97ea038.jpg",
-      },
-      item: {
-        name: "시우민 포스터 팔아여",
-      },
-      proposedPrice: 15000,
-      createdAt: 1627799751243,
-    },
-  ];
+  useEffect(() => {
+    const getNotifications = requestAuthData("v1/users/notifications");
+    getNotifications.then((result) => {
+      if (result === "login") {
+        setShowPopup(true);
+        return;
+      }
+      setNotifications(result);
+    });
+  }, []);
 
   return (
     <>
@@ -45,14 +27,13 @@ const NotificationPage = () => {
       {!showPopup && (
         <div>
           <HeaderInfo text="알림" padding="0 16px" />
-          <NotificationRowBox>
-            {notifications.map((notification) => (
-              <NotificationRow
-                key={notification.id}
-                notification={notification}
-              />
-            ))}
-          </NotificationRowBox>
+          {notifications !== null && (
+            <NotificationRowBox>
+              {notifications.map((notification, idx) => (
+                <NotificationRow key={idx} notification={notification} />
+              ))}
+            </NotificationRowBox>
+          )}
         </div>
       )}
     </>
@@ -60,7 +41,9 @@ const NotificationPage = () => {
 };
 
 const NotificationRowBox = styled.div`
-  margin-top: 60px;
+  overflow-y: auto;
+  height: 95vh;
+  margin-top: 40px;
   padding: 0 16px;
 `;
 
