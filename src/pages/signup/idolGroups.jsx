c3bccc1981e17b98df50c0593108eaee52327f0e
2dowon <dwon424@gmail.com>
2021-08-18 00:32:01 +0900

    ✨ [feat] implement validation of phone number by SMS

---
+++ src/pages/signup/idolGroups.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import styled from "styled-components";
+import styles from "./signup.module.css";
+
+import { requestPublicData } from "../../shared/axios";
+import { actionCreators as userActions } from "../../redux/modules/user";
+
+const IdolGroups = ({ onUpdate }) => {
+  const dispatch = useDispatch();
+  const idolValue = useSelector((state) => state.user.idolsForSignup);
+
+  // 아이돌 데이터 가져오기
+  const [idols, setIdols] = useState([]);
+
+  useEffect(() => {
+    const getIdolGroup = requestPublicData("v1/idol-groups");
+    getIdolGroup.then((result) => {
+      setIdols(result);
+    });
+  }, []);
+
+  if (!idols) {
+    return null;
+  }
+
+  const checkHandler = (id) => {
+    console.log(id);
+    onUpdate();
+    dispatch(userActions.setIdolsForSignup(id));
+  };
+
+  return (
+    <IdolList>
+      {idols.map((idol) => (
+        <IdolBox key={idol.id} onClick={() => checkHandler(`${idol.id}`)}>
+          <IdolInput
+            id={idol.id}
+            type="radio"
+            checked={idolValue === `${idol.id}`}
+            onChange={() => checkHandler(`${idol.id}`)}
+          />
+          <label
+            htmlFor={idol.id}
+            className={
+              idolValue === `${idol.id}` ? styles.selectedLabel : styles.label
+            }
+          >
+            <img
+              className={styles.idolGroupImg}
+              src={idol.imageUrl}
+              alt="Idol Group"
+            />
+            {idol.name}
+          </label>
+        </IdolBox>
+      ))}
+    </IdolList>
+  );
+};
+
+const IdolList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 85px);
+  grid-auto-rows: 115px;
+`;
+
+const IdolBox = styled.div``;
+
+const IdolInput = styled.input`
+  display: none;
+`;
+
+export default IdolGroups;
