391570362dd1360512b0511a02c9c5a9cd7f5263
2dowon <dwon424@gmail.com>
2021-09-06 00:37:21 +0900

    ✨ [feat] notification on/off

--- src/pages/myProfilePage/setting.jsx
+++ src/pages/myProfilePage/setting.jsx
@@ -1,37 +1,83 @@
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-no-duplicate-props */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 import styled from "styled-components";
+import styles from "./myProfilePage.module.css";
 
 import HeaderInfo from "../../components/haeder/headerInfo";
-import { Icon, Text } from "../../elements";
+import { Icon, Text, LoginPopUp } from "../../elements";
 import { blackBtn, darkRed, grayText } from "../../shared/colors";
+import { notification } from "../../shared/notification";
+import { requestAuthData, deleteAction } from "../../shared/axios";
 
 import { actionCreators as userActions } from "../../redux/modules/user";
 
-const Setting = (props) => {
+const Setting = () => {
   const dispatch = useDispatch();
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [isNotificationOn, setIsNotificationOn] = useState(true);
+
+  const requestUserData = async () => {
+    const result = await requestAuthData("v1/users/look-up");
+    if (result === "login") {
+      setShowPopup(true);
+    }
+    return result;
+  };
+  const fnEffect = async () => {
+    const userData = await requestUserData();
+    setIsNotificationOn(userData.isAgreeToNotification);
+  };
+  useEffect(fnEffect, []);
+
+  const notificationOn = () => {
+    setIsNotificationOn(true);
+
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ type: "REQ_FCM_TOKEN" }),
+      );
+    } else {
+      notification();
+    }
+  };
+  const notificationOff = () => {
+    setIsNotificationOn(false);
+    deleteAction("v1/users/device");
+  };
 
   return (
     <>
-      <HeaderInfo text="설정" />
-      <SettingBox>
-        {/* 사용자 설정 */}
-        <SettingBtn>
-          <Text color={grayText} size="16px" margin="0 0 15px 0">
-            사용자 설정
-          </Text>
-          <BtnBox>
-            <Text color={blackBtn} size="18px" medium>
-              알림 설정
-            </Text>
-            <Icon
-              width="12px"
-              src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
-            />
-          </BtnBox>
-          {/* <BtnBox>
+      {showPopup && <LoginPopUp />}
+      {!showPopup && (
+        <div>
+          <HeaderInfo text="설정" />
+          <SettingBox>
+            {/* 사용자 설정 */}
+            <SettingBtn>
+              <Text color={grayText} size="16px" margin="0 0 15px 0">
+                사용자 설정
+              </Text>
+              <BtnBox>
+                <Text color={blackBtn} size="18px" medium>
+                  알림 설정
+                </Text>
+                <label className={styles.switch}>
+                  <input
+                    type="checkbox"
+                    checked={isNotificationOn}
+                    onChange={() =>
+                      isNotificationOn ? notificationOff() : notificationOn()
+                    }
+                  />
+                  <span className={styles.slider} />
+                </label>
+              </BtnBox>
+              {/* <BtnBox>
             <Text color={blackBtn} size="18px" medium>
               배송지 및 계좌 설정
             </Text>
@@ -40,72 +86,74 @@ const Setting = (props) => {
               src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
             />
           </BtnBox> */}
-        </SettingBtn>
-        {/* 서비스 정보 */}
-        <SettingBtn>
-          <Text color={grayText} size="16px" margin="0px 0 15px 0">
-            서비스 정보
-          </Text>
-          <a href="https://soma-goodsduck.github.io/terms/service-policy.html">
-            <BtnBox>
-              <Text color={blackBtn} size="18px" medium>
-                이용 약관
+            </SettingBtn>
+            {/* 서비스 정보 */}
+            <SettingBtn>
+              <Text color={grayText} size="16px" margin="0px 0 15px 0">
+                서비스 정보
               </Text>
-              <Icon
-                width="12px"
-                src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
-              />
-            </BtnBox>
-          </a>
-          <a href="https://soma-goodsduck.github.io/terms/privacy.html">
-            <BtnBox>
-              <Text color={blackBtn} size="18px" medium>
-                개인정보 처리방침
-              </Text>
-              <Icon
-                width="12px"
-                src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
-              />
-            </BtnBox>
-          </a>
-          <a href="https://soma-goodsduck.github.io/terms/marketing-policy.html">
-            <BtnBox>
-              <Text color={blackBtn} size="18px" medium>
-                마케팅 정보 활용 동의
-              </Text>
-              <Icon
-                width="12px"
-                src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
-              />
-            </BtnBox>
-          </a>
-        </SettingBtn>
-        {/* 로그아웃 & 탈퇴 */}
-        <SettingBtn>
-          <BtnBox
-            onClick={() => {
-              dispatch(userActions.logoutAction());
-            }}
-          >
-            <Text color={blackBtn} size="18px" medium>
-              로그아웃
-            </Text>
-            <Icon
-              width="12px"
-              src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
-            />
-          </BtnBox>
-          <BtnBox>
-            <Text color={blackBtn} size="18px" medium color={darkRed}>
-              회원탈퇴
-            </Text>
-            <Icon
-              width="12px"
-              src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more_red.svg"
-            />
-          </BtnBox>
-        </SettingBtn>
-      </SettingBox>
+              <a href="https://soma-goodsduck.github.io/terms/service-policy.html">
+                <BtnBox>
+                  <Text color={blackBtn} size="18px" medium>
+                    이용 약관
+                  </Text>
+                  <Icon
+                    width="12px"
+                    src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
+                  />
+                </BtnBox>
+              </a>
+              <a href="https://soma-goodsduck.github.io/terms/privacy.html">
+                <BtnBox>
+                  <Text color={blackBtn} size="18px" medium>
+                    개인정보 처리방침
+                  </Text>
+                  <Icon
+                    width="12px"
+                    src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
+                  />
+                </BtnBox>
+              </a>
+              <a href="https://soma-goodsduck.github.io/terms/marketing-policy.html">
+                <BtnBox>
+                  <Text color={blackBtn} size="18px" medium>
+                    마케팅 정보 활용 동의
+                  </Text>
+                  <Icon
+                    width="12px"
+                    src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
+                  />
+                </BtnBox>
+              </a>
+            </SettingBtn>
+            {/* 로그아웃 & 탈퇴 */}
+            <SettingBtn>
+              <BtnBox
+                onClick={() => {
+                  dispatch(userActions.logoutAction());
+                }}
+              >
+                <Text color={blackBtn} size="18px" medium>
+                  로그아웃
+                </Text>
+                <Icon
+                  width="12px"
+                  src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more.svg"
+                />
+              </BtnBox>
+              <BtnBox>
+                <Text color={blackBtn} size="18px" medium color={darkRed}>
+                  회원탈퇴
+                </Text>
+                <Icon
+                  width="12px"
+                  src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_more_red.svg"
+                />
+              </BtnBox>
+            </SettingBtn>
+          </SettingBox>
+        </div>
+      )}
     </>
   );
 };
