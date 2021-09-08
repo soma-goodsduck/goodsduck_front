4d98c7e3023c59e3a538e214fa8a119b82634fbd
2dowon <dwon424@gmail.com>
2021-09-08 18:09:22 +0900

    ✨ [feat] apple login for iOS

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
 
 import styled from "styled-components";
+
 import { Flex, Button } from "../../elements";
 import {
   NAVER_AUTH_URL_PROD,
   NAVER_AUTH_URL_DEV,
   KAKAO_AUTH_URL,
+  APPLE_AUTH_URL,
 } from "../../shared/OAuth";
 import { history } from "../../redux/configureStore";
 
-const Login = (props) => {
+const Login = () => {
+  const userAgent = window.navigator.userAgent;
+  const isIOSApp = userAgent.indexOf("IOS APP");
+
   useEffect(() => {
     localStorage.removeItem("jwt");
     localStorage.removeItem("likeIdolGroups");
@@ -18,51 +23,77 @@ const Login = (props) => {
   }, []);
 
   return (
-    <Flex is_col>
+    <LoginBox>
       <Logo />
-      <Flex is_flex>
-        <a
-          href={
-            process.env.REACT_APP_TYPE === "DEV"
-              ? NAVER_AUTH_URL_DEV
-              : NAVER_AUTH_URL_PROD
-          }
-        >
-          <Button
-            width="60px"
-            height="60px"
-            borderRadius="50%"
-            margin="0 20px 0 0"
-            src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_naver.svg"
-          />
-        </a>
-        <a href={KAKAO_AUTH_URL}>
-          <Button
-            width="60px"
-            height="60px"
-            borderRadius="50%"
-            margin="0 20px 0 0"
-            src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_kakao.svg"
-          />
-        </a>
-      </Flex>
-      <Flex is_flex margin="20px">
-        <TextBtn
-          onClick={() => {
-            history.replace("/");
-          }}
-        >
-          그냥 둘러볼게요
-        </TextBtn>
-      </Flex>
-    </Flex>
+      <div>
+        <Flex is_flex>
+          <a
+            href={
+              process.env.REACT_APP_TYPE === "DEV"
+                ? NAVER_AUTH_URL_DEV
+                : NAVER_AUTH_URL_PROD
+            }
+          >
+            <Button
+              width="60px"
+              height="60px"
+              borderRadius="50%"
+              margin="0 20px 0 0"
+              src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_naver.svg"
+            />
+          </a>
+          <a href={KAKAO_AUTH_URL}>
+            <Button
+              width="60px"
+              height="60px"
+              borderRadius="50%"
+              margin="0 20px 0 0"
+              src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_kakao.svg"
+            />
+          </a>
+          {isIOSApp !== -1 && (
+            <a href={APPLE_AUTH_URL}>
+              <Button
+                width="60px"
+                height="60px"
+                borderRadius="50%"
+                margin="0 20px 0 0"
+                src="https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/icon_apple.png"
+              />
+            </a>
+          )}
+        </Flex>
+        <Flex is_flex margin="20px">
+          <TextBtn
+            onClick={() => {
+              history.replace("/");
+            }}
+          >
+            그냥 둘러볼게요
+          </TextBtn>
+        </Flex>
+      </div>
+    </LoginBox>
   );
 };
+
+const LoginBox = styled.div`
+  width: 100vw;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20vh;
+
+  @media screen and (min-width: 415px) {
+    width: 415px;
+  }
+`;
 
 const Logo = styled.div`
   width: 80%;
   height: 70px;
-  margin: 250px auto;
   background-image: url("https://goodsduck-s3.s3.ap-northeast-2.amazonaws.com/icon/logo.svg");
   background-size: contain;
   background-repeat: no-repeat;
--- src/shared/OAuth2RedirectHandler.js
+++ src/shared/OAuth2RedirectHandler.js
@@ -1,19 +1,33 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import axios from "axios";
 import * as Sentry from "@sentry/react";
 
-import { useDispatch } from "react-redux";
 import { actionCreators as userActions } from "../redux/modules/user";
 
 import { notification } from "./notification";
 import Spinner from "./spinner";
 
 const OAuth2RedirectHandler = () => {
+  const userAgent = window.navigator.userAgent;
+  const isIOSApp = userAgent.indexOf("IOS APP");
+
   const dispatch = useDispatch();
   const href = window.location.href;
   const params = new URL(document.location).searchParams;
   const code = params.get("code");
   const state = params.get("state");
+  let appleCode;
+  let appleToken;
+
+  if (isIOSApp !== -1) {
+    const appleHref = window.location.href.split("&");
+    const appleCodeHref = appleHref[appleHref.length - 2].split("=");
+    appleCode = appleCodeHref[appleCodeHref.length - 1];
+    const appleTokenHref = appleHref[appleHref.length - 1].split("=");
+    appleToken = appleTokenHref[appleTokenHref.length - 1];
+  }
 
   const kakaoLogin = async () => {
     try {
@@ -87,11 +101,48 @@ const OAuth2RedirectHandler = () => {
     }
   };
 
+  const appleLogin = async () => {
+    console.log(appleCode, appleToken);
+
+    try {
+      const result = await axios.get(
+        `${process.env.REACT_APP_BACK_URL}/api/v1/users/login/apple?code=${appleCode}&idToken=${appleToken}&state=${process.env.REACT_APP_APPLE_STATE}`,
+      );
+
+      console.log(result);
+      if (result.data.response.role === "ANONYMOUS") {
+        dispatch(
+          userActions.nonUserAction(
+            result.data.response.socialAccountId,
+            "APPLE",
+          ),
+        );
+      } else {
+        if (result.data.response.isAgreeToNotification) {
+          if (window.ReactNativeWebView) {
+            window.ReactNativeWebView.postMessage(
+              JSON.stringify({ type: "REQ_FCM_TOKEN" }),
+            );
+          } else {
+            notification();
+          }
+        }
+
+        dispatch(userActions.loginAction(result.data.response.jwt));
+      }
+    } catch (error) {
+      console.log("error", error);
+      Sentry.captureException(error);
+    }
+  };
+
   useEffect(() => {
     if (href.includes("/kakao")) {
       kakaoLogin();
     } else if (href.includes("/naver")) {
       _naverLogin();
+    } else if (href.includes("/apple")) {
+      appleLogin();
     }
   });
 
